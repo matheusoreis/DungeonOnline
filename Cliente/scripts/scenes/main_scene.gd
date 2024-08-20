a0b8018abd725721eb01e0842e0ea7e0c058e497
Matheus Reis <matheuss_reis@outlook.com>
2024-08-19 22:33:23 -0300

    Main

--- Cliente/scripts/scenes/main_scene.gd
+++ Cliente/scripts/scenes/main_scene.gd
@@ -8,52 +8,52 @@ var _scene_tree: SceneTree
 
 
 func _ready() -> void:
-    _scene_tree = get_tree()
-    _client = Network.websocket
-    _constants = Constants.new()
-    _processor = Processor.new(_scene_tree)
+	_scene_tree = get_tree()
+	_client = Network.websocket
+	_constants = Constants.new()
+	_processor = Processor.new(_scene_tree)
 
-    _connect_to_server()
-    _client.packet_received.connect(_on_packet_received)
-    _client.connected.connect(_on_connected)
-    _client.disconnected.connect(_on_disconnected)
+	_connect_to_server()
+	_client.packet_received.connect(_on_packet_received)
+	_client.connected.connect(_on_connected)
+	_client.disconnected.connect(_on_disconnected)
 
 
 func _process(delta) -> void:
-    if _client != null:
-        _client.poll()
+	if _client != null:
+		_client.poll()
 
 
 func _connect_to_server() -> void:
-    var result = await _client.connect_to_host(_constants.host, _constants.port)
+	var result = await _client.connect_to_host(_constants.host, _constants.port)
 
-    if result != OK:
-        _on_disconnected()
+	if result != OK:
+		_on_disconnected()
 
 
 func _on_connected() -> void:
-    var alert_scene := preload("res://scenes/ui/alert/alert_ui.tscn") as PackedScene
-    var alert_panel := alert_scene.instantiate() as AlertUI
-    var alert_panel_location := '/root/main/Shared/AlertPanel/AlertsVBox'
-    var alert_vbox := _scene_tree.root.get_node(alert_panel_location) as VBoxContainer
-    alert_panel.message.text = 'Conectado ao servidor!'
-    alert_vbox.add_child(alert_panel)
+	var alert_scene := preload("res://scenes/ui/alert/alert_ui.tscn") as PackedScene
+	var alert_panel := alert_scene.instantiate() as AlertUI
+	var alert_panel_location := '/root/main/Shared/AlertPanel/AlertsVBox'
+	var alert_vbox := _scene_tree.root.get_node(alert_panel_location) as VBoxContainer
+	alert_panel.message.text = 'Conectado ao servidor!'
+	alert_vbox.add_child(alert_panel)
 
 
 func _on_packet_received(data: PackedByteArray) -> void:
-    if data.size() == 0:
-        _client.disconnect_from_host()
-        return
+	if data.size() == 0:
+		_client.disconnect_from_host()
+		return
 
-    var packet := Packet.new()
-    packet = packet.from_packed_byte_array(data)
-    _processor.process_message(packet)
+	var packet := Packet.new()
+	packet = packet.from_packed_byte_array(data)
+	_processor.process_message(packet)
 
 
 func _on_disconnected() -> void:
-    var alert_scene := preload("res://scenes/ui/alert/alert_ui.tscn") as PackedScene
-    var alert_panel := alert_scene.instantiate() as AlertUI
-    var alert_panel_location := '/root/main/Shared/AlertPanel/AlertsVBox'
-    var alert_vbox := _scene_tree.root.get_node(alert_panel_location) as VBoxContainer
-    alert_panel.message.text = 'Não foi possível se conectar ao servidor!'
-    alert_vbox.add_child(alert_panel)
+	var alert_scene := preload("res://scenes/ui/alert/alert_ui.tscn") as PackedScene
+	var alert_panel := alert_scene.instantiate() as AlertUI
+	var alert_panel_location := '/root/main/Shared/AlertPanel/AlertsVBox'
+	var alert_vbox := _scene_tree.root.get_node(alert_panel_location) as VBoxContainer
+	alert_panel.message.text = 'Não foi possível se conectar ao servidor!'
+	alert_vbox.add_child(alert_panel)
